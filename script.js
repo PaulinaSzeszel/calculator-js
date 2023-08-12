e6f4360f09c14eadb6795298651c6c39f01f99a1
Mauricio Pernia <mauriciopernia67@gmail.com>
2023-08-12 12:15:59 -0400

    fixed Dot x2

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
-let input = document.getElementById('inputBox')
-let buttons = document.querySelectorAll('button')
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+let dotButton = document.getElementById('dot'); // Agregamos esta línea
 
-let string = ''
-let arr = Array.from(buttons)
+let string = '';
+let arr = Array.from(buttons);
 
-function checkDot(input, button) {
-  if (input.value.includes(".")) {
-    button.disabled = true;
+function updateDotButton() {
+  if (string.includes('.')) {
+    dotButton.disabled = true;
   } else {
-    button.disabled = false;
+    dotButton.disabled = false;
   }
 }
 
 arr.forEach((button) => {
   button.addEventListener('click', (e) => {
-    checkDot(input, document.getElementById("dot"));
     if (e.target.innerHTML == '=') {
-      string = eval(string)
-      input.value = string
+      string = eval(string);
+      input.value = string;
     } else if (e.target.innerHTML == 'AC') {
-      string = ''
-      input.value = string
+      string = '';
+      input.value = string;
     } else if (e.target.innerHTML == 'DEL') {
-      string = string.substring(0, string.length - 1)
-      input.value = string
+      string = string.substring(0, string.length - 1);
+      input.value = string;
+    } else if (e.target.innerHTML == '.') {
+      if (!string.endsWith('.')) {
+        string += e.target.innerHTML;
+        input.value = string;
+      }
     } else {
-      string += e.target.innerHTML
-      input.value = string
+      string += e.target.innerHTML;
+      input.value = string;
     }
-  })
-})
+    
+    updateDotButton(); // Llamamos a la función para actualizar el estado del botón de punto
+  });
+});
+
+dotButton.addEventListener('click', () => {
+  if (!string.includes('.')) {
+    string += '.';
+    input.value = string;
+  }
+  
+  updateDotButton();
+});
