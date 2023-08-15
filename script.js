5a4a974eda60a3b24af41e201951a1a8b0e5b2ed
Paulina Szeszel Web Developer <86908540+PaulinaSzeszel@users.noreply.github.com>
2023-08-15 19:34:13 +0200

    fixed entire project

--- script.js
+++ script.js
@@ -1,48 +1,277 @@
-let input = document.getElementById('inputBox');
-let buttons = document.querySelectorAll('button');
-let dotButton = document.getElementById('dot');
+const inputElement = document.querySelector('.current')
+const fullOperationElement = document.querySelector('.previous')
+const numbers = document.querySelectorAll('.number')
+const operators = document.querySelectorAll('.operator')
+const clearButton = document.querySelector('.clear')
+const toggleSign = document.querySelector('.toggle')
+const percentage = document.querySelector('.percentage')
+const equalsButton = document.querySelector('.equals')
 
-let string = '';
-let arr = Array.from(buttons);
-
-function updateDotButton() {
-  if (string.includes('.')) {
-    dotButton.disabled = true;
-  } else {
-    dotButton.disabled = false;
-  }
+const fullOperation = {
+  prev: '',
+  current: '',
+  operation: '',
 }
 
-arr.forEach((button) => {
-  button.addEventListener('click', (e) => {
-    if (e.target.innerHTML == '=') {
-      string = eval(string);
-      input.value = string;
-    } else if (e.target.innerHTML == 'AC') {
-      string = '';
-      input.value = string;
-    } else if (e.target.innerHTML == 'DEL') {
-      string = string.substring(0, string.length - 1);
-      input.value = string;
-    } else if (e.target.innerHTML == '.') {
-      if (!string.endsWith('.')) {
-        string += e.target.innerHTML;
-        input.value = string;
-      }
-    } else {
-      string += e.target.innerHTML;
-      input.value = string;
-    }
-    
-    updateDotButton();
-  });
-});
+const clear = () => {
+  fullOperation.prev = ''
+  fullOperation.current = ''
+  fullOperation.operation = ''
+  inputElement.innerText = ''
+  fullOperationElement.innerText = ''
+  return
+}
 
-dotButton.addEventListener('click', () => {
-  if (!string.includes('.')) {
-    string += '.';
-    input.value = string;
+const backspace = () => {
+  let input = fullOperation.current.toString()
+  if (input === '') {
+    return
   }
-  
-  updateDotButton();
-});
+  input = input.slice(0, -1)
+  if (input.length === 0) {
+    fullOperation.current = ''
+    return
+  }
+  fullOperation.current = parseFloat(input)
+  return
+}
+
+const updateDisplay = () => {
+  inputElement.innerText = fullOperation.current.toString()
+  fullOperationElement.innerText =
+    fullOperation.prev.toString() +
+    (fullOperation.operation.toString() !== ''
+      ? ' ' + fullOperation.operation.toString()
+      : '')
+  return
+}
+
+const appendNumber = (number) => {
+  if (fullOperation.prev && fullOperation.operation === '') {
+    fullOperation.prev = ''
+  }
+  let input = fullOperation.current.toString()
+  if (input.length > 9) {
+    return
+  }
+  if (number === '.' && input.includes('.')) {
+    return
+  }
+  if (number === '.' && input === '') {
+    number = '0.'
+  }
+  if (input === '0' && number !== '.') {
+    input = number
+    fullOperation.current = input
+    return
+  }
+  input = input + number
+  fullOperation.current = input
+  return
+}
+
+const chooseOperation = (operator) => {
+  if (
+    fullOperation.current === '' &&
+    fullOperation.prev === '' &&
+    fullOperation.operation === ''
+  ) {
+    return
+  } else if (
+    fullOperation.prev &&
+    fullOperation.operation === '' &&
+    fullOperation.current === ''
+  ) {
+    fullOperation.operation = operator
+  } else if (
+    fullOperation.prev &&
+    fullOperation.operation &&
+    fullOperation.current === ''
+  ) {
+    fullOperation.operation = operator
+    return
+  } else if (
+    fullOperation.operation !== '' &&
+    fullOperation.prev !== '' &&
+    fullOperation.current !== ''
+  ) {
+    result = compute()
+    fullOperation.prev = result
+    fullOperation.operation = operator
+    fullOperation.current = ''
+  } else {
+    fullOperation.prev = fullOperation.current
+    fullOperation.operation = operator
+    fullOperation.current = ''
+  }
+
+  return
+}
+
+const compute = () => {
+  if (
+    fullOperation.prev === '' ||
+    fullOperation.current === '' ||
+    fullOperation.operation === ''
+  ) {
+    return
+  }
+  if (isNaN(fullOperation.prev) || isNaN(fullOperation.current)) {
+    return
+  }
+  fullOperation.current = parseFloat(fullOperation.current)
+  fullOperation.prev = parseFloat(fullOperation.prev)
+
+  let computation = ''
+
+  switch (fullOperation.operation) {
+    case '+':
+      computation = fullOperation.prev + fullOperation.current
+      break
+    case '-':
+      computation = fullOperation.prev - fullOperation.current
+      break
+    case 'x':
+      computation = fullOperation.prev * fullOperation.current
+      break
+    case '÷':
+      computation = fullOperation.prev / fullOperation.current
+      break
+    default:
+      return
+  }
+
+  // if (computation.toString().length > 9 && computation.toString().includes(".")) {
+  // }
+  computation = Math.round((computation + Number.EPSILON) * 100) / 100
+
+  return computation
+}
+
+const evaluate = () => {
+  if (
+    fullOperation.current === '' ||
+    fullOperation.prev === '' ||
+    fullOperation.operation === ''
+  ) {
+    return
+  }
+  if (fullOperation.current === '0' && fullOperation.operation === '÷') {
+    clear()
+    inputElement.innerText = '🤯'
+    return
+  }
+  result = compute()
+  fullOperation.prev = result
+  fullOperation.operation = ''
+  fullOperation.current = ''
+  return
+}
+
+const changeSign = () => {
+  if (fullOperation.current === '') {
+    return
+  }
+  fullOperation.current = parseFloat(fullOperation.current * -1)
+  return
+}
+
+const calculatePercentage = () => {
+  if (fullOperation.current === '') {
+    return
+  }
+  if (fullOperation.current.toString().includes('.')) {
+    return
+  }
+  fullOperation.current = fullOperation.current / 100
+  return
+}
+
+numbers.forEach((number) => {
+  number.addEventListener('click', () => {
+    appendNumber(number.innerText)
+    updateDisplay()
+  })
+})
+
+operators.forEach((operator) => {
+  operator.addEventListener('click', () => {
+    chooseOperation(operator.innerText)
+    updateDisplay()
+  })
+})
+
+equalsButton.addEventListener('click', () => {
+  evaluate()
+  if (inputElement.innerText === '🤯') {
+    return
+  }
+  updateDisplay()
+})
+
+clearButton.addEventListener('click', () => {
+  clear()
+  updateDisplay()
+})
+
+toggleSign.addEventListener('click', () => {
+  changeSign()
+  updateDisplay()
+})
+
+percentage.addEventListener('click', () => {
+  calculatePercentage()
+  updateDisplay()
+})
+
+document.addEventListener('keydown', (event) => {
+  const key = event.key
+
+  //single digit number keys
+  if (/^\d$/.test(key)) {
+    appendNumber(key)
+    updateDisplay()
+  }
+  if (key === '.') {
+    appendNumber(key)
+    updateDisplay()
+  }
+
+  //operators + - * /
+  if (/[+\-\*/]/.test(key)) {
+    if (key === '*') {
+      chooseOperation('x')
+      updateDisplay()
+    } else if (key === '/') {
+      chooseOperation('÷')
+      updateDisplay()
+    } else {
+      chooseOperation(key)
+      updateDisplay()
+    }
+  }
+
+  if (key === 'Enter') {
+    evaluate()
+    updateDisplay()
+  }
+
+  if (key === 'Escape') {
+    clear()
+    updateDisplay()
+  }
+
+  if (key === 'Backspace') {
+    backspace()
+    updateDisplay()
+  }
+
+  if (key === '%') {
+    calculatePercentage()
+    updateDisplay()
+  }
+  if (key === 'Shift') {
+    changeSign()
+    updateDisplay()
+  }
+})
